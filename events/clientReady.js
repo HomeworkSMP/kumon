5896ef47a59058b72db152f13b802ec7965d867b
Acto <acto@actoweb.xyz>
2025-11-08 21:35:39 -0800

    Update bot presence messages and interval

--- events/clientReady.js
+++ events/clientReady.js
@@ -9,19 +9,19 @@ module.exports = {
 		setInterval(() => {
 			switch (Math.floor(Math.random() * 3)) {
 			case 0:
-				bot.user.setPresence({ activities: [{ name: "HomeworkSMP", type: 0 }], status: "idle" });
+				bot.user.setPresence({ activities: [{ name: "Playing on HomeworkSMP", type: 0 }], status: "idle" });
 				break;
 			case 1:
-				bot.user.setPresence({ activities: [{ name: "Mud's Non-Sense", type: 2 }], status: "idle" });
+				bot.user.setPresence({ activities: [{ name: "Listening to Mud's Non-Sense", type: 2 }], status: "idle" });
 				break;
 			case 2:
-				bot.user.setPresence({ activities: [{ name: "the player count", type: 3 }], status: "idle" });
+				bot.user.setPresence({ activities: [{ name: "Watching the player count", type: 3 }], status: "idle" });
 				break;
 			default:
-				bot.user.setPresence({ activities: [{ name: "HomeworkSMP", type: 0 }], status: "idle" });
+				bot.user.setPresence({ activities: [{ name: "Playing on HomeworkSMP", type: 0 }], status: "idle" });
 				break;
 			}
-		}, 600000);
+		}, 60000);
 		const getLastActoMessage = async function() {
 			const hwsmpChatMsgs = await (await bot.channels.fetch("1223705840335196182")).messages.fetch({ limit: 100 });
 			return await hwsmpChatMsgs.filter(m => m.author.id == "428445352354643968").first();
